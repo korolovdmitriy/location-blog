aa292eb84f6b2b2d803a30f5d184c5cbe0603a3d
Dmytro Korolov <84042045+korolovdmitriy@users.noreply.github.com>
2023-05-08 17:30:57 +0300

    add user card

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
-function Users () {
-  return <h2>Users Works</h2>;
-};
+function Users() {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Dmytro",
+      image:
+        "https://akns-images.eonline.com/eol_images/Entire_Site/20221115/rs_1200x1200-221215142043-avatar-5.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+}
 
 export default Users;
