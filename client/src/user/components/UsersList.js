b4b684ffa61ff35e7f3e5ded0ad86db9f9a6aad3
Dmytro Korolov <84042045+korolovdmitriy@users.noreply.github.com>
2023-05-08 20:47:31 +0300

    add header

--- client/src/user/components/UsersList.js
+++ client/src/user/components/UsersList.js
@@ -1,19 +1,22 @@
 import React from "react";
-import "./UsersList";
-import "./UserItem";
+
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
 
 function UsersList(props) {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
 
   return (
-    <ul>
+    <ul className="users-list">
       {props.items.map((user) => (
         <UserItem
           key={user.id}
